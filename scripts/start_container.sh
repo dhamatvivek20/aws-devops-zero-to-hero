7a4fd1febc7aecb3e6742b70e8a478629bec3c1f
dhamatvivek20 <117385213+dhamatvivek20@users.noreply.github.com>
2024-03-06 15:57:19 +0530

    Update start_container.sh
    file changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull dhamatvivek/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 dhamatvivek/simple-python-flask-app
